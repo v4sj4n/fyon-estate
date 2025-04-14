3776090e5b668e30ada16fa9ef66dab0b1c536c4
Vasjan Çupri <vasjancupri04@gmail.com>
2025-04-14 17:07:58 +0200

    Added cloudflare to the viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import path from "path";
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +12,7 @@ export default defineConfig({
     react(),
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     tailwindcss(),
+    cloudflare()
   ],
   resolve: {
     alias: {
